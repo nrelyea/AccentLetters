ef0dadca832df64cf2c7b7e4f25b9b53121bd3cf
Nathan Relyea <relyean19@up.edu>
2019-10-02 20:25:22 -0700

    Initial Commit: Buttons on panel

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Accents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a_button = new System.Windows.Forms.Button();
+            this.e_button = new System.Windows.Forms.Button();
+            this.i_button = new System.Windows.Forms.Button();
+            this.o_button = new System.Windows.Forms.Button();
+            this.u_button = new System.Windows.Forms.Button();
+            this.n_button = new System.Windows.Forms.Button();
+            this.uu_button = new System.Windows.Forms.Button();
+            this.exc_button = new System.Windows.Forms.Button();
+            this.que_button = new System.Windows.Forms.Button();
+            this.change_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // a_button
+            // 
+            this.a_button.Font = new System.Drawing.Font("Arial Narrow", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a_button.Location = new System.Drawing.Point(12, 12);
+            this.a_button.Name = "a_button";
+            this.a_button.Size = new System.Drawing.Size(150, 150);
+            this.a_button.TabIndex = 0;
+            this.a_button.Text = "á";
+            this.a_button.UseVisualStyleBackColor = true;
+            // 
+            // e_button
+            // 
+            this.e_button.Font = new System.Drawing.Font("Arial Narrow", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.e_button.Location = new System.Drawing.Point(168, 12);
+            this.e_button.Name = "e_button";
+            this.e_button.Size = new System.Drawing.Size(150, 150);
+            this.e_button.TabIndex = 1;
+            this.e_button.Text = "é";
+            this.e_button.UseVisualStyleBackColor = true;
+            // 
+            // i_button
+            // 
+            this.i_button.Font = new System.Drawing.Font("Arial Narrow", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.i_button.Location = new System.Drawing.Point(324, 12);
+            this.i_button.Name = "i_button";
+            this.i_button.Size = new System.Drawing.Size(150, 150);
+            this.i_button.TabIndex = 2;
+            this.i_button.Text = "í";
+            this.i_button.UseVisualStyleBackColor = true;
+            // 
+            // o_button
+            // 
+            this.o_button.Font = new System.Drawing.Font("Arial Narrow", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.o_button.Location = new System.Drawing.Point(480, 12);
+            this.o_button.Name = "o_button";
+            this.o_button.Size = new System.Drawing.Size(150, 150);
+            this.o_button.TabIndex = 3;
+            this.o_button.Text = "ó";
+            this.o_button.UseVisualStyleBackColor = true;
+            // 
+            // u_button
+            // 
+            this.u_button.Font = new System.Drawing.Font("Arial Narrow", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.u_button.Location = new System.Drawing.Point(636, 12);
+            this.u_button.Name = "u_button";
+            this.u_button.Size = new System.Drawing.Size(150, 150);
+            this.u_button.TabIndex = 4;
+            this.u_button.Text = "ú";
+            this.u_button.UseVisualStyleBackColor = true;
+            // 
+            // n_button
+            // 
+            this.n_button.Font = new System.Drawing.Font("Arial Narrow", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n_button.Location = new System.Drawing.Point(12, 168);
+            this.n_button.Name = "n_button";
+            this.n_button.Size = new System.Drawing.Size(150, 150);
+            this.n_button.TabIndex = 5;
+            this.n_button.Text = "ñ";
+            this.n_button.UseVisualStyleBackColor = true;
+            // 
+            // uu_button
+            // 
+            this.uu_button.Font = new System.Drawing.Font("Arial Narrow", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uu_button.Location = new System.Drawing.Point(168, 168);
+            this.uu_button.Name = "uu_button";
+            this.uu_button.Size = new System.Drawing.Size(150, 150);
+            this.uu_button.TabIndex = 6;
+            this.uu_button.Text = "ü";
+            this.uu_button.UseVisualStyleBackColor = true;
+            // 
+            // exc_button
+            // 
+            this.exc_button.Font = new System.Drawing.Font("Arial Narrow", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exc_button.Location = new System.Drawing.Point(324, 168);
+            this.exc_button.Name = "exc_button";
+            this.exc_button.Size = new System.Drawing.Size(150, 150);
+            this.exc_button.TabIndex = 7;
+            this.exc_button.Text = "¡";
+            this.exc_button.UseVisualStyleBackColor = true;
+            // 
+            // que_button
+            // 
+            this.que_button.Font = new System.Drawing.Font("Arial Narrow", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.que_button.Location = new System.Drawing.Point(480, 168);
+            this.que_button.Name = "que_button";
+            this.que_button.Size = new System.Drawing.Size(150, 150);
+            this.que_button.TabIndex = 8;
+            this.que_button.Text = "¿";
+            this.que_button.UseVisualStyleBackColor = true;
+            // 
+            // change_button
+            // 
+            this.change_button.Font = new System.Drawing.Font("Arial Narrow", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.change_button.Location = new System.Drawing.Point(636, 168);
+            this.change_button.Name = "change_button";
+            this.change_button.Size = new System.Drawing.Size(150, 150);
+            this.change_button.TabIndex = 9;
+            this.change_button.Text = "Change\r\nCase\r\n";
+            this.change_button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.change_button);
+            this.Controls.Add(this.que_button);
+            this.Controls.Add(this.exc_button);
+            this.Controls.Add(this.uu_button);
+            this.Controls.Add(this.n_button);
+            this.Controls.Add(this.u_button);
+            this.Controls.Add(this.o_button);
+            this.Controls.Add(this.i_button);
+            this.Controls.Add(this.e_button);
+            this.Controls.Add(this.a_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a_button;
+        private System.Windows.Forms.Button e_button;
+        private System.Windows.Forms.Button i_button;
+        private System.Windows.Forms.Button o_button;
+        private System.Windows.Forms.Button u_button;
+        private System.Windows.Forms.Button n_button;
+        private System.Windows.Forms.Button uu_button;
+        private System.Windows.Forms.Button exc_button;
+        private System.Windows.Forms.Button que_button;
+        private System.Windows.Forms.Button change_button;
+    }
+}
+
